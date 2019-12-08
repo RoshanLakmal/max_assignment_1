918d5e71264494aba5c528d1f14cab45f19429a4
Roshan <roshanrox911@gmail.com>
2019-12-08 20:48:28 +1100

    added userinput and useroutput components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [{ username: 'Roshan' }, { username: 'Lakmal' }]
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Test</h1>
+        <UserOutput username="Roshan"></UserOutput>
+        <UserOutput username="Lakmal"></UserOutput>
       </div>
     );
   }
---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './UserInput.css';
+
+const UserInput = props => {
+  <div className="UserInput">
+    <input type="text"></input>
+  </div>;
+};
+
+export default UserInput;
